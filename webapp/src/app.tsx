5eaff170243b991739c1fb68382e5090dc74e326
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2020-03-18 08:29:32 +0900

    Show channel list

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -18,7 +18,19 @@ import '@/app.css';
 import {ChatDataSourceImpl} from '@/data/api/chat-data-source-impl';
 import {ChatRepository} from '@/data/repository/chat-repository';
 import {ChannelID, ChatChannel, ChatComment, CommentID} from '@/model/chat-models';
-import {Fabric, initializeIcons, List, mergeStyles, Stack, Text, TextField} from 'office-ui-fabric-react';
+import {
+    Fabric,
+    INavLink,
+    INavLinkGroup,
+    initializeIcons,
+    List,
+    mergeStyles,
+    Nav,
+    Stack,
+    Text,
+    TextField
+} from 'office-ui-fabric-react';
+import 'office-ui-fabric-react/dist/css/fabric.min.css'
 import {
     CSSProperties,
     default as React,
@@ -26,9 +38,9 @@ import {
     KeyboardEvent,
     useCallback,
     useEffect,
+    useMemo,
     useRef,
     useState,
-    useMemo,
 } from 'react';
 
 initializeIcons();
@@ -89,20 +101,44 @@ const renderCell = (item?: ChatComment, index?: number, isScrolling?: boolean): 
 const getCommentKey = (item?: ChatComment, index?: number): string => item ? item.commentID : 'none';
 
 const listContainerClass = mergeStyles({
-    overflow: 'scroll',
+    overflowX: 'hidden',
+    overflowY: 'auto',
 });
+
+const calcWindowSize = (): Partial<CSSProperties> => ({
+    position: 'absolute',
+    top: 0,
+    bottom: window.outerHeight - window.innerHeight,
+    left: 0,
+    right: window.outerWidth - window.innerWidth,
+});
+
+const useWindowSize = () => {
+    const createState = () => ({
+        outerWidth: window.outerWidth,
+        outerHeight: window.outerHeight,
+        innerWidth: window.innerWidth,
+        innerHeight: window.innerHeight,
+    });
+    const [windowSize, setWindowSize] = useState(createState());
+
+    useEffect(() => {
+        const cb = () => setWindowSize(createState());
+        window.addEventListener('resize', cb);
+        return () => window.removeEventListener('resize', cb);
+    }, []);
+    return windowSize;
+};
 
 const App = (): FunctionComponentElement<unknown> => {
     const chat = useMemo(() => new ChatRepository(new ChatDataSourceImpl(process.env.GRAPHQL_ENDPOINT)), []);
+    const [allChannels, setAllChannels] = useState<ChatChannel[]>([]);
     const [currentChannel, setCurrentChannel] = useState<ChatChannel>();
     const [comments, setComments] = useState<ChatComment[]>([]);
     const initialUserName = localStorage.getItem('userName');
     const [userName, setUserName] = useState(initialUserName ? initialUserName : '');
     const [message, setMessage] = useState('');
-    const [windowSize, setWindowSize] = useState<CSSProperties>({
-        width: window.innerWidth,
-        height: window.innerHeight,
-    });
+    const windowSize = useWindowSize();
     const [requestScrollCommentListToBottom, setRequestScrollCommentListToBottom] = useState(false);
     const refCommentList = useRef<List<ChatComment>>(null);
 
@@ -147,6 +183,10 @@ const App = (): FunctionComponentElement<unknown> => {
                 setComments(prev => prev.concat(data));
                 window.setTimeout(() => polling(channelID, data[data.length - 1].commentID), 0);
             } catch (e) {
+                if (e.name == 'AbortError') {
+                    console.log(`abort retrieveCommentsWithLongPolling`);
+                    return;
+                }
                 const timeout = retryCounter.timeoutMilliSec();
                 console.log(`failed to execute retrieveCommentsWithLongPolling. retry: ${timeout}ms, reason: ${e}`);
                 window.setTimeout(() => polling(channelID, commentID), timeout);
@@ -171,6 +211,10 @@ const App = (): FunctionComponentElement<unknown> => {
                     }
                 }
             } catch (e) {
+                if (e.name == 'AbortError') {
+                    console.log(`abort retrieveComments`);
+                    return;
+                }
                 const timeout = retryCounter.timeoutMilliSec();
                 console.log(`failed to execute retrieveComments. retry: ${timeout}ms, reason: ${e}`);
                 window.setTimeout(task, timeout);
@@ -199,17 +243,18 @@ const App = (): FunctionComponentElement<unknown> => {
         const retryCounter = new RetryCounter();
         let timeoutHandle: number | undefined = undefined;
         const abortController = new AbortController();
-        const task = async(): Promise<void> => {
+        const task = async (): Promise<void> => {
             try {
                 const channels = await chat.retrieveChannels(abortController.signal);
                 retryCounter.reset();
-                if (channels.length == 0) {
-                    console.log(`TODO:`);
+                console.log(channels);
+                setAllChannels(channels);
+            } catch (e) {
+                if (e.name == 'AbortError') {
+                    console.log(`abort retrieveChannels`);
                     return;
                 }
-                setCurrentChannel(channels[0]);
-            } catch (e) {
-                const timeout =  retryCounter.timeoutMilliSec();
+                const timeout = retryCounter.timeoutMilliSec();
                 console.log(`failed to execute retrieveChannels. retry: ${timeout}ms`);
                 timeoutHandle = window.setTimeout(task, timeout);
             }
@@ -226,6 +271,14 @@ const App = (): FunctionComponentElement<unknown> => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    useEffect(() => {
+        if (allChannels.length == 0) {
+            console.log(`TODO:`);
+            return;
+        }
+        setCurrentChannel(allChannels[0]);
+    }, [allChannels, setCurrentChannel]);
+
     useEffect(() => localStorage.setItem("userName", userName), [userName]);
     const noMessageKeyDown = useCallback((ev: KeyboardEvent) => {
         if (ev.key == 'Enter') {
@@ -238,34 +291,52 @@ const App = (): FunctionComponentElement<unknown> => {
         }
     }, [onSendClick, message]);
 
-    useEffect(() => {
-        const windowSizeCb = () => setWindowSize({
-            position: 'absolute',
-            top: 0,
-            bottom: window.outerHeight - window.innerHeight,
-            left: 0,
-            right: window.outerWidth - window.innerWidth,
-        });
-        window.addEventListener('resize', windowSizeCb);
-        return () => window.removeEventListener('resize', windowSizeCb);
-    }, []);
+    const channelListItem = useMemo<INavLinkGroup[]>(() => [{
+        links: allChannels.map((data) => ({
+            name: data.name,
+            url: '',
+            key: data.channelID,
+        }))
+    }], [allChannels]);
+
+    const onClickNav = useCallback((ev?: React.MouseEvent<HTMLElement>, item?: INavLink): void => {
+        if (!item) {
+            return;
+        }
+
+        const channel = allChannels.find((data) => data.channelID == item.key);
+        if (channel) {
+            setCurrentChannel(channel);
+        }
+    }, [allChannels, setCurrentChannel]);
 
     return (
         <Fabric>
-            <Stack style={windowSize}>
-                <div className={listContainerClass} data-is-scrollable="true">
-                    <List ref={refCommentList} getKey={getCommentKey} items={comments} onRenderCell={renderCell}/>
+            <div className='ms-Grid' dir='ltr' style={{
+                width: windowSize.innerWidth,
+                height: windowSize.innerHeight
+            }}>
+                <div className='ms-Grid-row'>
+                    <div className='ms-Grid-col ms-hiddenLgDown ms-xl2'>
+                        <Nav groups={channelListItem} onLinkClick={onClickNav} selectedKey={currentChannel?.channelID}/>
+                    </div>
+                    <div className='ms-Grid-col ms-lg12 ms-xl10' style={{padding: 0}}>
+                        <div className={listContainerClass} data-is-scrollable="true"
+                             style={{width: windowSize.innerWidth, height: `calc(${windowSize.innerHeight}px - 9rem`}}>
+                            <List ref={refCommentList} getKey={getCommentKey} items={comments}
+                                  onRenderCell={renderCell}/>
+                        </div>
+                        <div style={{height: '9rem'}}>
+                            <TextField style={{fontSize: '16px'}} label={'name'} defaultValue={userName}
+                                       onChange={(e: any, value?: string) => setUserName(value ? value : '')}/>
+                            <TextField style={{fontSize: '16px'}} label={'message'}
+                                       onChange={(e: any, value?: string) => setMessage(value ? value : '')}
+                                       onKeyDown={noMessageKeyDown} value={message}
+                                       description={'return to send'}/>
+                        </div>
+                    </div>
                 </div>
-                <div style={{width: '100%', alignSelf: 'flex-end'}}>
-                    <Stack tokens={{childrenGap: 15}} style={{margin: '16px'}}>
-                        <TextField style={{fontSize: '16px'}} label={'name'} defaultValue={userName}
-                                   onChange={(e: any, value?: string) => setUserName(value ? value : '')}/>
-                        <TextField style={{fontSize: '16px'}} label={'message'}
-                                   onChange={(e: any, value?: string) => setMessage(value ? value : '')}
-                                   onKeyDown={noMessageKeyDown} value={message} description={'return to send'}/>
-                    </Stack>
-                </div>
-            </Stack>
+            </div>
         </Fabric>
     );
 };
